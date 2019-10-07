d28c845b9283383f87c4497496feb73ad1c96a51
abhijithmahipal <abhijithmahipal@gmail.com>
2019-10-07 23:16:33 +0530

    added config in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAubCjoaE1kkQ9WKMaUCzwC6zbiBCBHJQ4",
+    authDomain: "firestore-angular-eaeaf.firebaseapp.com",
+    databaseURL: "https://firestore-angular-eaeaf.firebaseio.com",
+    projectId: "firestore-angular-eaeaf",
+    storageBucket: "firestore-angular-eaeaf.appspot.com",
+    messagingSenderId: "1023455999696",
+    appId: "1:1023455999696:web:9391264fde895ccf63d0d5",
+    measurementId: "G-P4W4XBS7HL"
+  }
 };
