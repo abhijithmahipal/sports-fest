e3c84d29e3e271ae344008205756e91b63030f33
ArjunKaruthodi <arjunkaruthodi@gmail.com>
2020-03-30 21:34:38 +0530

    Replace nfig by ngswitch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,31 +7,30 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
-
   carousalData: any[] = new Array<any>();
   responsiveOptions: any;
 
   constructor() {
     this.carousalData.push({
-      isVideo: true,
+      carousalType: "video",
       source: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/e481beb9627f70c3a57589ae05165ed6_screen.mp4?ts=1584858457',
       poster: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/e481beb9627f70c3a57589ae05165ed6_screen.jpg?ts=1584858457',
       type: 'video/mp4'
     });
     this.carousalData.push({
-      isVideo: false,
+      carousalType: "image",
       source: '../../assets/em11.jpg'
     });
     this.carousalData.push({
-      isVideo: false,
+      carousalType: "image",
       source: '../../assets/ehiring.jpg'
     });
     this.carousalData.push({
-      isVideo: false,
+      carousalType: "image",
       source: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/26363223d4dc8f5ce673428802ea988a_screen.jpg?ts=1583388529'
     });
     this.carousalData.push({
-      isVideo: false,
+      carousalType: "image",
       source: '../../assets/md10.jpg'
     });
 
