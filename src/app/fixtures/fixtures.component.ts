3b2976a7a03e78b3afc9dd6675f16dffd0177b80
Abhijith Mahipal <abhijithm@valorem.com>
2020-06-03 22:55:20 +0530

    fixture styled

--- src/app/fixtures/fixtures.component.ts
+++ src/app/fixtures/fixtures.component.ts
@@ -7,7 +7,7 @@ import { NgxSpinnerService } from "ngx-spinner";
 @Component({
   selector: "fixtures",
   templateUrl: "./fixtures.component.html",
-  styleUrls: ["./fixtures.component.css"],
+  styleUrls: ["./fixtures.component.scss"],
 })
 export class FixturesComponent implements OnInit {
   fixtures: Fixture[];
@@ -27,13 +27,21 @@ export class FixturesComponent implements OnInit {
         (a) => a.date && a.date.substring(4)
       );
       this.datewiseFixture = _.chain(this.fixtures)
-        .groupBy('date')
+        .groupBy("date")
         .toArray()
         .value();
-        this.spinner.hide();
+      this.spinner.hide();
     });
   }
+  getHome(game: string): string {
+    let array = game && game.split('vs');
+    return array && array[0].trim();
+  }
 
+  getAway(game: string): string {
+    let array = game && game.split('vs');
+    return array && array[1].trim();
+  }
   //   for (let index = 0; index < this.fixtures.length; index++) {
   //     this.teamService.addFixture(this.fixtures[index]);
   //   }
