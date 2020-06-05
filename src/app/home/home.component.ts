cab96140a860daca8323bc520b4d6a41a245d0cf
Abhijith Mahipal <abhijithm@valorem.com>
2020-06-05 16:55:24 +0530

    homepage-cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
